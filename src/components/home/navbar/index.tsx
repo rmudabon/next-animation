a4e9156ba4152db4b515b7d4de9e047afb18e52f
Rowmel Marco Dabon <rmdabs@gmail.com>
2024-05-03 13:03:44 +0800

    feat: navbar styling

--- src/components/home/navbar/index.tsx
+++ src/components/home/navbar/index.tsx
@@ -1,19 +1,42 @@
+'use client';
 
-'use client'
-
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const Navbar = () => (
-    <NavigationMenu>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <Link href='#about' legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            About
-          </NavigationMenuLink>
+  <NavigationMenu className='fixed'>
+    <div className='w-full max-w-7xl flex space-x-5'>
+      <Image src='/vercel.svg' alt='Logo' width={100} height={100} />
+      <NavigationMenuList>
+        <NavigationMenuItem className='h-full'>
+          <Link href='/' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
           </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
+        </NavigationMenuItem>
+        <NavigationMenuItem className='h-full'>
+          <Link href='#about' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem className='h-full'>
+          <Link href='#gifts' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Gifts
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </div>
   </NavigationMenu>
-)
+);
